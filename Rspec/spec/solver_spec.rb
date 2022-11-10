2cad2759482fa9307c83060af0e712ba1a3199cb
Duane David <duanedave17@gmail.com>
2022-11-10 05:56:51 -0800

    Add test for fizzbuzz method

--- Rspec/spec/solver_spec.rb
+++ Rspec/spec/solver_spec.rb
@@ -1,8 +1,8 @@
 require_relative './../../solver'
 
 describe Solver do
-  context "When testing the Solver class" do
-    it "should return the factorial of number passed to the factorial method of class Solver" do
+  context 'When testing the Solver class' do
+    it 'should return the factorial of number passed to the factorial method of class Solver' do
       solver = Solver.new
       expect(solver.factorial(0)).to eq 1
       expect(solver.factorial(1)).to eq 1
@@ -12,11 +12,21 @@ describe Solver do
     end
   end
 
-  context "When testing the Solver class" do
-    it "should return a reversed string passed to the reverse method e.g. \"hello\" should return \"olleh\" of class Solver" do
+  context 'When testing the Solver class' do
+    it 'should return a reversed string passed to the reverse method e.g. "hello" should return "olleh" of class Solver' do
       solver = Solver.new
       expect(solver.reverse('hello')).to eq 'olleh'
       expect(solver.reverse('william')).to eq 'mailliw'
+    end
+  end
+
+  context 'when testing the fizzbuzz method' do
+    it 'Should return a string when fizzbuzz method is called with an intger' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(1)).to eq '1'
+      expect(solver.fizzbuzz(3)).to eq 'fizz'
+      expect(solver.fizzbuzz(5)).to eq 'buzz'
+      expect(solver.fizzbuzz(15)).to eq 'fizzbuzz'
     end
   end
 end
